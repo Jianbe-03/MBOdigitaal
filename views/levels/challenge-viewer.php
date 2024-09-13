f3832b00025e83dda91a1deddac902b51901417f
Jibbe Winands <jibbewinands@outlook.com>
2024-09-13 13:00:54 +0200

    Comments translated to dutch

--- views/levels/challenge-viewer.php
+++ views/levels/challenge-viewer.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/database/dbconnection.php';
 
-// Query to get all levels with associated deliverable IDs
+// Query om alle levels met bijbehorende deliverable-ID's op te halen
 $sql = "SELECT id, educationId, level, subject, description, deliverables FROM levels";
 $stmt = $db->prepare($sql);
 $stmt->execute();
@@ -11,25 +11,25 @@ echo "<div class='mt-6 mb-16 w-11/12 p-6 space-y-8 sm:p-8 bg-white mx-auto'>
     <h2 class='text-2xl font-bold dark:text-black'>Levels</h2>
     <p class='my-4 font-bold text-gray-700'>Hieronder staan alle levels en hun bijhorende informatie</p>";
 
-// Loop through each level
+// Loop door elk level
 foreach ($levels as $level) {
-    // Extract deliverable IDs
+    // Haal de deliverable-ID's uit elkaar
     $deliverableIds = explode(',', $level['deliverables']);
     
-    // Get deliverables from the database
-    $inQuery = implode(',', array_fill(0, count($deliverableIds), '?')); // prepare placeholders
+    // Query om deliverables uit de database op te halen
+    $inQuery = implode(',', array_fill(0, count($deliverableIds), '?')); // voorbereiden van placeholders
     $deliverablesStmt = $db->prepare("SELECT id, DeliverableName, DeliverableDesc FROM deliverables WHERE id IN ($inQuery)");
-    $deliverablesStmt->execute($deliverableIds);
+    $deliverablesStmt->execute($deliverableIds); // Uitvoeren van de query met de deliverable-ID's
     $deliverables = $deliverablesStmt->fetchAll(PDO::FETCH_ASSOC);
     
-    // Display level information and create a button for each level
+    // Toon de level-informatie en maak een knop voor elk level
     echo "<div class='level'>
             <h3 class='text-xl font-semibold text-gray-800'>Level: {$level['level']}</h3>
             <p>{$level['description']}</p>
-            <button class='toggle-deliverables' data-level-id='{$level['id']}'>View Deliverables</button>
+            <button class='toggle-deliverables' data-level-id='{$level['id']}'>Bekijk Deliverables</button>
             <ul id='deliverables-{$level['id']}' class='hidden'>";
     
-    // Display deliverables for the level
+    // Toon de deliverables voor het huidige level
     foreach ($deliverables as $deliverable) {
         echo "<li>
                 <button class='toggle-description' data-deliverable-id='{$deliverable['id']}'>{$deliverable['DeliverableName']}</button>
